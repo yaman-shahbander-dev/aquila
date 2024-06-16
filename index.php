6f9e50c05e833f0f426f6fd5d71150ed4c2969a0
yaman <yaman.dev3@gmail.com>
2024-06-16 13:19:53 +0300

    theme's first files

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Main template file
+ * 
+ * @package Aquila
+ * 
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Wordpress Theme</title>
+</head>
+<body>
+</body>
+</html>
