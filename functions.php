b8f2a53fdd7046837a3268f7b00f460ac3cf3c14
yaman <yaman.dev3@gmail.com>
2024-06-19 21:31:09 +0300

    add bootstrap

--- functions.php
+++ functions.php
@@ -8,11 +8,21 @@
 
 
 function aquila_enqueue_scripts() {
+	// Register Styles
 	wp_register_style('style-css', get_stylesheet_uri());
-	wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', [], false, true);
+	wp_register_style('bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css');
 
+	// Register Scripts
+	wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', [], false, true);
+	wp_register_script('bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true);
+
+	// Enqueue Styles
 	wp_enqueue_style('style-css');
+	wp_enqueue_style('bootstrap-css');
+
+	// Enqueue Scripts
 	wp_enqueue_script('main-js');
+	wp_enqueue_script('bootstrap-js');
 }
 
 add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
--- header.php
+++ header.php
@@ -15,5 +15,7 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<?php wp_body_open(); ?>
+	<?php 
+		if (function_exists('wp_body_open')) wp_body_open(); 
+	?>
 	<header>Header</header>
